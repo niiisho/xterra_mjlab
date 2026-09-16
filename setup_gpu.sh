5e34f279755bcada613f52b8c3cf647422f0c0f3
Nishant Garg <248533326+niiisho@users.noreply.github.com>
2026-09-16 21:24:54 +0530

    Create setup_gpu.sh

---
+++ setup_gpu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Installing main framework..."
+pip install -e .
+
+echo "Fixing dependency conflicts (wandb, tensorboard, numpy, scipy)..."
+pip install wandb==0.16.6 tensorboard==2.15.1 numpy==1.26.4 scipy==1.12.0
+
+echo "Environment ready for training!"
