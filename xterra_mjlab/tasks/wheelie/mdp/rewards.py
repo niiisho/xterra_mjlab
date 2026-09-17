d4feb01791d788994483657211c088c6658c093c
Nishant Garg <248533326+niiisho@users.noreply.github.com>
2026-09-17 09:00:30 +0530

    Update base_height_too_low to use body_pos_w

--- xterra_mjlab/tasks/wheelie/mdp/rewards.py
+++ xterra_mjlab/tasks/wheelie/mdp/rewards.py
@@ -72,5 +72,5 @@ def excessive_roll(env, max_roll: float = 0.785) -> torch.Tensor:
     return roll_signal > max_roll  # bool tensor, no .float()
 
 def base_height_too_low(env, min_height: float):
-    # env.root_states[:, 2] is the Z-height of the robot's center of mass
-    return env.root_states[:, 2] < min_height
+    # body_pos_w contains all links. [:, 0, 2] means: all environments, 0th body (the base), Z-axis (height)
+    return env.scene["robot"].data.body_pos_w[:, 0, 2] < min_height
