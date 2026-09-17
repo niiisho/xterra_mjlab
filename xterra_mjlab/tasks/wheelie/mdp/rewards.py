c273a2f8879cc2dd4d204a9b39653ff40b17c911
Nishant Garg <248533326+niiisho@users.noreply.github.com>
2026-09-17 12:37:55 +0530

    Update rewards.py

--- xterra_mjlab/tasks/wheelie/mdp/rewards.py
+++ xterra_mjlab/tasks/wheelie/mdp/rewards.py
@@ -66,10 +66,8 @@ def excessive_roll(env, max_roll: float = 0.785) -> torch.Tensor:
 
 def base_height_too_low(env, min_height: float, grace_period: int):
     # Check if the body dropped below the limit
-    is_low = env.scene["robot"].data.body_com_pos_w[:, 0, 2] < min_height
-    past_grace = env.episode_length_buf > grace_period
-    
-    return is_low & past_grace
+    is_low = env.scene["robot"].data.body_com_pos_w[:, 0, 2] < min_height    
+    return is_low
 
 def non_foot_contact_penalty(
     env,
