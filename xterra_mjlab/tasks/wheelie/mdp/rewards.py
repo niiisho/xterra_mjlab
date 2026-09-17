6cca03dfe5cae89bbe9037f17323437c80cc366b
Nishant Garg <248533326+niiisho@users.noreply.github.com>
2026-09-17 08:58:09 +0530

    Update rewards.py

--- xterra_mjlab/tasks/wheelie/mdp/rewards.py
+++ xterra_mjlab/tasks/wheelie/mdp/rewards.py
@@ -72,4 +72,5 @@ def excessive_roll(env, max_roll: float = 0.785) -> torch.Tensor:
     return roll_signal > max_roll  # bool tensor, no .float()
 
 def base_height_too_low(env, min_height: float):
-    return env.scene["robot"].data.root_pos_w[:, 2] < min_height
+    # body_pos_w contains all links. [:, 0, 2] means: all environments, 0th body (the base), Z-axis (height)
+    return env.scene["robot"].data.body_pos_w[:, 0, 2] < min_height
