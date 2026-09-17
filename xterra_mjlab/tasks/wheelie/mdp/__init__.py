ee4204f2663ea13b46a1e9a97411eadeac81cfc9
Nishant Garg <248533326+niiisho@users.noreply.github.com>
2026-09-17 23:00:13 +0530

    Update __init__.py

--- xterra_mjlab/tasks/wheelie/mdp/__init__.py
+++ xterra_mjlab/tasks/wheelie/mdp/__init__.py
@@ -3,7 +3,7 @@ from .rewards import (
     excessive_roll,
     base_height_too_low,
     illegal_contact_fall,
-    front_continuous_air_reward,
+    front_air_height_reward,
     front_contact_penalty,
     base_height_penalty,
 )
