bab99c95f5035152dbb4d09cf4f1192e1f0b1e4b
Nishant Garg <248533326+niiisho@users.noreply.github.com>
2026-09-17 11:09:07 +0530

    Update __init__.py

--- xterra_mjlab/tasks/wheelie/mdp/__init__.py
+++ xterra_mjlab/tasks/wheelie/mdp/__init__.py
@@ -2,4 +2,7 @@ from .rewards import (
     front_wheelie_reward,
     rear_wheelie_reward,
     excessive_roll,
+    non_foot_contact_penalty,
+    survival_reward,
 )
+from .events import reset_to_wheelie_pose
