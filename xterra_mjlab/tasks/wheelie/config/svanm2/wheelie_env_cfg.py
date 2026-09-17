098e1c3ebbb6b55d094d256304b480135dc71a1e
Nishant Garg <248533326+niiisho@users.noreply.github.com>
2026-09-17 10:46:05 +0530

    Update wheelie_env_cfg.py

--- xterra_mjlab/tasks/wheelie/config/svanm2/wheelie_env_cfg.py
+++ xterra_mjlab/tasks/wheelie/config/svanm2/wheelie_env_cfg.py
@@ -79,6 +79,7 @@ def svanm2_front_wheelie_cfg(play: bool = False):
         params={
             "sensor_name": "feet_ground_contact",
             "target_pitch": 0.5,
+            "grace_steps": 150,
         },
     )
 
