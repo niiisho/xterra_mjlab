5e6574f0ff076441e831b06640b573e572ae91f5
Nishant Garg <248533326+niiisho@users.noreply.github.com>
2026-09-19 08:44:16 +0530

    Update setup_gpu.sh

--- setup_gpu.sh
+++ setup_gpu.sh
@@ -3,6 +3,7 @@ echo "Installing main framework..."
 pip install -e .
 
 echo "Fixing dependency conflicts (wandb, tensorboard, numpy, scipy)..."
+pip install --upgrade pip setuptools wheel
 pip install wandb==0.16.6 tensorboard==2.15.1 numpy==1.26.4 scipy==1.12.0 "protobuf==4.25.3" "onnx==1.16.1" wandb
 
 echo "Environment ready for training!"
