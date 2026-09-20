1107c2208c9f0c41f87d2b4ce2d01432ddc1208f
Nishant Garg <248533326+niiisho@users.noreply.github.com>
2026-09-20 19:45:58 +0530

    Update __init__.py

--- xterra_mjlab/tasks/wheelie/mdp/__init__.py
+++ xterra_mjlab/tasks/wheelie/mdp/__init__.py
@@ -12,5 +12,6 @@ from .rewards import (
     lateral_out_of_bounds,
     min_velocity_penalty,
     rear_knee_posture_penalty,
+    front_leg_direction_penalty,
 )
 from .events import reset_to_wheelie_pose
