175aba4e909f685552529060497f6fa6fddd7a86
Nishant Garg <248533326+niiisho@users.noreply.github.com>
2026-09-22 16:28:04 +0530

    Update __init__.py

--- xterra_mjlab/tasks/wheelie/mdp/__init__.py
+++ xterra_mjlab/tasks/wheelie/mdp/__init__.py
@@ -8,9 +8,6 @@ from .rewards import (
     base_height_penalty,
     yaw_rate_penalty,
     front_symmetry_penalty,
-    forward_velocity_reward,
-    lateral_out_of_bounds,
-    min_velocity_penalty,
     rear_knee_posture_penalty,
     front_leg_direction_penalty,
 )
