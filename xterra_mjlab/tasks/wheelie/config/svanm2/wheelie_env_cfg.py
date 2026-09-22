78d93999aa089ac8f0ddaa6c7bda823ca676ddd2
Nishant Garg <248533326+niiisho@users.noreply.github.com>
2026-09-22 18:35:58 +0530

    Update wheelie_env_cfg.py

--- xterra_mjlab/tasks/wheelie/config/svanm2/wheelie_env_cfg.py
+++ xterra_mjlab/tasks/wheelie/config/svanm2/wheelie_env_cfg.py
@@ -82,7 +82,7 @@ def svanm2_front_wheelie_cfg(play: bool = False):
     cfg.terminations["base_too_low"] = TerminationTermCfg(
         func=wheelie_mdp.base_height_too_low,
         params={
-            "min_height": 0.32,
+            "min_height": 0.34,
             "grace_period": 30
         },
     )
@@ -106,7 +106,7 @@ def svanm2_front_wheelie_cfg(play: bool = False):
         func=wheelie_mdp.base_height_penalty,
         weight=-2.0,
         params={
-            "penalty_threshold": 0.37,
+            "penalty_threshold": 0.38,
         },
     )
 
@@ -115,7 +115,7 @@ def svanm2_front_wheelie_cfg(play: bool = False):
         weight=5.0,  # A flat 5.0 points per frame if it holds the high wheelie
         params={
             "sensor_name": "feet_ground_contact",
-            "min_height": 0.4
+            "min_height": 0.42
         },
     )
 
