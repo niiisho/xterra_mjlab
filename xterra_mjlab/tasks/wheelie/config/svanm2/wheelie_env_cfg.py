6f38ed3fbd0e988370123335cf18d4bdefe5a465
Nishant Garg <248533326+niiisho@users.noreply.github.com>
2026-09-23 22:20:00 +0530

    Update wheelie_env_cfg.py

--- xterra_mjlab/tasks/wheelie/config/svanm2/wheelie_env_cfg.py
+++ xterra_mjlab/tasks/wheelie/config/svanm2/wheelie_env_cfg.py
@@ -248,8 +248,8 @@ def svanm2_rear_wheelie_cfg(play: bool = False):
 def svanm2_wheelie_stairs_cfg(play: bool = False):
     cfg = svanm2_flat_env_cfg(play=play)
     
-    cfg.sim.nconmax = 2000  # Increase maximum allowed contacts
-    cfg.sim.njmax = 4000
+    cfg.sim.nconmax = 600  # Increase maximum allowed contacts
+    cfg.sim.njmax = 200
 
     # 1. TERRAIN GENERATOR: Straight Stairs
     # Set the type string to tell the framework to expect a generator
