a5710bd0d6ab2b42f89fd29eb857e6e71e1a70f3
Nishant Garg <248533326+niiisho@users.noreply.github.com>
2026-09-23 17:02:34 +0530

    Update __init__.py

--- xterra_mjlab/tasks/wheelie/config/svanm2/__init__.py
+++ xterra_mjlab/tasks/wheelie/config/svanm2/__init__.py
@@ -3,12 +3,13 @@
 from mjlab.tasks.registry import register_mjlab_task
 from mjlab.tasks.velocity.rl import VelocityOnPolicyRunner
 
-from .wheelie_env_cfg import svanm2_front_wheelie_cfg, svanm2_rear_wheelie_cfg, svanm2_front_wheelie_fromflat_cfg
+from .wheelie_env_cfg import svanm2_front_wheelie_cfg, svanm2_rear_wheelie_cfg, svanm2_front_wheelie_fromflat_cfg, svanm2_wheelie_stairs_cfg
 from .rl_cfg import svanm2_wheelie_ppo_runner_cfg
 
 FRONT_WHEELIE_TASK = "xTerra-Mjlab-Wheelie-Front-SvanM2"
 FRONT_WHEELIE_FROMFLAT_TASK = "xTerra-Mjlab-Wheelie-Front-FromFlat-SvanM2"
 REAR_WHEELIE_TASK = "xTerra-Mjlab-Wheelie-Rear-SvanM2"
+STAIR_WHEELIE_TASK = "xTerra-Mjlab-Stairs-Wheelie-SvanM2"
 
 register_mjlab_task(
     task_id=FRONT_WHEELIE_TASK,
@@ -30,6 +31,14 @@ register_mjlab_task(
     task_id=REAR_WHEELIE_TASK,
     env_cfg=svanm2_rear_wheelie_cfg(),
     play_env_cfg=svanm2_rear_wheelie_cfg(play=True),
+    rl_cfg=svanm2_wheelie_ppo_runner_cfg(),
+    runner_cls=VelocityOnPolicyRunner,
+)
+
+register_mjlab_task(
+    task_id=STAIR_WHEELIE_TASK,
+    env_cfg=svanm2_wheelie_stairs_cfg(),
+    play_env_cfg=svanm2_wheelie_stairs_cfg(play=True),
     rl_cfg=svanm2_wheelie_ppo_runner_cfg(),
     runner_cls=VelocityOnPolicyRunner,
 )
