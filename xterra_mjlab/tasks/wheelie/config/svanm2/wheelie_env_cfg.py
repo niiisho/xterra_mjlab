e9281d58d3fce7cd4513034174d9d1fb64137d22
Nishant Garg <248533326+niiisho@users.noreply.github.com>
2026-09-25 10:09:32 +0530

    Update wheelie_env_cfg.py

--- xterra_mjlab/tasks/wheelie/config/svanm2/wheelie_env_cfg.py
+++ xterra_mjlab/tasks/wheelie/config/svanm2/wheelie_env_cfg.py
@@ -255,7 +255,7 @@ def svanm2_wheelie_stairs_cfg(play: bool = False):
     cfg.scene.terrain.terrain_generator = TerrainGeneratorCfg(
         curriculum=True, 
         # Restored to 15x15 to prevent the engine from rotating the world!
-        size=(15.0, 15.0), 
+        size=(40.0, 40.0), 
         sub_terrains={
             "straight_stairs": HfStraightStairsCfg(
                 step_height=0.1, 
